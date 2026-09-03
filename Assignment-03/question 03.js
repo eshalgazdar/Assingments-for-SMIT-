006a56ee5575426622578f9a392ac13aa6d1a9f9
eshalgazdar <eshalarsalangazdar@gmail.com>
2026-09-03 23:57:58 +0500

    Add files via upload

---
+++ Assignment-03/question 03.js
@@ -0,0 +1,56 @@
+var studentName = "Eshal";
+
+let age = 18;
+
+const university = "Bahria University";
+
+console.log("var studentName:", studentName);
+console.log("let age:", age);
+console.log("const university:", university);
+
+
+// DATA TYPES
+
+let name = "Eshal";
+console.log("String:", name);
+console.log("Type:", typeof name);
+
+let marks = 95;
+console.log("Number:", marks);
+console.log("Type:", typeof marks);
+
+let passed = true;
+console.log("Boolean:", passed);
+console.log("Type:", typeof passed);
+
+let result;
+console.log("Undefined:", result);
+console.log("Type:", typeof result);
+
+let middleName = null;
+console.log("Null:", middleName);
+console.log("Type:", typeof middleName);
+
+let student = {
+    name: "Eshal",
+    age: 18,
+    program: "Computer Engineering"
+};
+
+console.log("Object:", student);
+console.log("Type:", typeof student);
+
+let subjects = ["HTML", "CSS", "JavaScript"];
+
+console.log("Array:", subjects);
+console.log("Type:", typeof subjects);
+
+let id = Symbol("studentID");
+
+console.log("Symbol:", id);
+console.log("Type:", typeof id);
+
+let largeNumber = 12345678901234567890;
+
+console.log("BigInt:", largeNumber);
+console.log("Type:", typeof largeNumber);
